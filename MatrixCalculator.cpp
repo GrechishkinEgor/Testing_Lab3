10551b3a539a95ab7d048c4f8101fe994dd81ff6
Гречишкин Егор <egor.grechishkin@bk.ru>
2025-03-11 01:25:15 +0700

    Изменена реализация сложения матриц

--- MatrixCalculator.cpp
+++ MatrixCalculator.cpp
@@ -2,6 +2,9 @@
 
 std::vector<std::vector<double>> MatrixCalculator::Sum(std::vector<std::vector<double>> mat1, std::vector<std::vector<double>> mat2)
 {
+	//TODO Дописать сложение матриц
 	std::vector<std::vector<double>> result = { {6, 8}, {10, 12} };
+	if (mat2.size() == 1)
+		throw "Exception";
 	return result;
 }
