1a5f9d7b8849f1c62591dfd0bb1ea9e5ce933f53
Гречишкин Егор <egor.grechishkin@bk.ru>
2025-03-19 23:05:49 +0700

    Создан тестирующий класс

---
+++ MatrixCalculatorTests/MatrixCalculatorTests.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MatrixCalculatorTests
+{
+	TEST_CLASS(MatrixCalculatorTests)
+	{
+	public:
+		
+	};
+}
